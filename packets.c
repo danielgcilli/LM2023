cf85326a8cad1d77ccd1cf3acc02910c2290fa04
Alex Clevenger <alexbclevenger@gmail.com>
2023-06-21 16:07:46 -0400

    developed packets from documentation

---
+++ packets.c
@@ -0,0 +1,18 @@
+#include "packets.h"
+typedef unsigned char byte;
+
+byte *syn_stream(byte *ip_header, byte *tcp_header, byte *ether_frame){
+    
+}
+
+byte *serialize_ip_header(IP_HEADER *ip_header){
+
+}
+
+byte *serialize_tcp_header(TCP_HEADER *tcp_header){
+
+}
+
+byte *serialize_ether(ETHER *eth_frame){
+
+}
--- packets.h
+++ packets.h
@@ -1,16 +1,16 @@
 #ifndef PACKETS_H
 #define PACKETS_H
 
-
-typedef unsigned char byte;
+#include <stdint.h>
 
 struct ethernet_segment {
     uint8_t mac_dst[6];
-    uint8_t mac_dst[6];
+    uint8_t mac_src[6];
     uint8_t protocol_type[2]; 
 };
+typedef struct ethernet_segment ETHER;
 
-struct ip_segment {
+struct ip_header {
     uint8_t version_n_IHL;
     uint8_t type_of_service;
     uint16_t total_length;
@@ -21,19 +21,22 @@ struct ip_segment {
     uint16_t header_checksum;
     uint32_t src_address;
     uint32_t dst_address;
-    // no data
-};
+    // leaving out options and padding for SYN packet
+}; 
+typedef struct ip_header IP_HEADER;
 
-struct tcp_segment {
+struct tcp_header {
     uint16_t src_port;
     uint16_t dst_port;
     uint32_t sequence_num;
     uint32_t ack_num;
-    
+    uint8_t offset_n_reserved;
+    uint8_t control_bits;
+    uint16_t window;
+    uint16_t urgent_ptr;
+    // leaving out options for SYN packet
 };
+typedef struct tcp_header TCP_HEADER;
 
-struct tcp_packet {
-
-};
 
 #endif
