74aa35ed99fba7fe7e858933d7598c08b86db9e5
Alex Clevenger <alexbclevenger@gmail.com>
2023-07-10 13:35:50 -0400

    new structure

---
+++ pineapple/main.py
@@ -0,0 +1,20 @@
+# MAC header breakdown
+# 1. Frame Control (2 byte)
+# 2. Duration (2 byte)
+# 3. DA (6 byte)
+# 4. SA (6 byte)
+# 5. BSS ID (6 byte)
+# 6. Seq-ctl (2 byte) 
+# optional...
+
+# Beacon Frame breakdown
+# 1. Timestamp (8 byte)
+# 2. Beacon Interval (2 byte)
+# 3. Capability info (2 byte)
+# 4. SSID (variable size)
+# 5. Supported Rates (variable size)
+# optional...
+
+# Hardware additions at the ethernet layer are bypassed via packet injection
+# libpcap equivalent in c
+
